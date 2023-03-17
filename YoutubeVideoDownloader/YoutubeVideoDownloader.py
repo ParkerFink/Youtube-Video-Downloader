e943c6ec6da5ba0d4d5e7a3fa6c392ac96c1346a
FinkParker <parkeririshgod@gmail.com>
2023-03-17 18:34:51 -0400

    exit, clear added
    added 'clear' and 'exit' functions along with adding documentation for them.

--- YoutubeVideoDownloader/YoutubeVideoDownloader.py
+++ YoutubeVideoDownloader/YoutubeVideoDownloader.py
@@ -3,10 +3,35 @@ from pytube import YouTube
 from cmd import Cmd
 
 
+import os
+
+
+
 class Terminal(Cmd):
 
     def do_download(self, url):
         yt = YouTube(url).streams.get_highestResolution().download('videos/')
 
+
+    def help_download(self):
+        print("It is used to download a single video at a time.")
+        print("EX: download https://www.youtube.com/watch?v=DYcLFHgVCn0")
+
+
+
+    def do_clear(self, blank):
+        os.system('cls')
+
+
+    def help_clear(self):
+        print("Clears all text in the terminal.")
+
+
+    def do_exit(self, blank):
+        exit()
+
+    def help_exit(self):
+        print("Closes Youtube Video Downloader.")
+    
 
 Terminal().cmdloop()
